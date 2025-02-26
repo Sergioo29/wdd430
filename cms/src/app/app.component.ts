b0302f927d5378615b153d9738ac6b57610c2d3c
Sergio <sergiohds_29@hotmail.com>
2025-02-25 21:29:05 -0700

    routes working

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
 
 @Component({
-  selector: 'cms-root',
+  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, ContactsComponent, DocumentsComponent, MessageListComponent], 
+  imports: [RouterModule, CommonModule, HeaderComponent, ContactsComponent, DocumentsComponent, MessageListComponent], 
+  template: `<router-outlet></router-outlet>`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- cms/src/app/app.routes.ts
+++ cms/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DocumentsComponent } from './documents/documents.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/documents', pathMatch: 'full' },
+  { path: 'documents', component: DocumentsComponent },
+  { path: 'messages', component: MessageListComponent },
+  { path: 'contacts', component: ContactsComponent }
+];
