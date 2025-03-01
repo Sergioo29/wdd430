b2deb3c8d05a8447ee9ec46df708cfb2df65b635
Sergio <sergiohds_29@hotmail.com>
2025-02-28 20:57:25 -0700

    doc edit component created

--- cms/src/app/app.routes.ts
+++ cms/src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { ContactsComponent } from './contacts/contacts.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/documents', pathMatch: 'full' },
-  { path: 'documents', component: DocumentsComponent },
+  {
+    path: 'documents',
+    component: DocumentsComponent,
+    children: [
+      { path: 'new', component: DocumentEditComponent }, // Create a new document
+      { path: ':id', component: DocumentDetailComponent }, // View a document
+      { path: ':id/edit', component: DocumentEditComponent } // Edit a document
+    ]
+  },
   { path: 'messages', component: MessageListComponent },
   { path: 'contacts', component: ContactsComponent }
 ];
