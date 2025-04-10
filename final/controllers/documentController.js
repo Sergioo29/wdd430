92358f8d1666070800ae957066b359e08813e184
Sergio <sergiohds_29@hotmail.com>
2025-04-09 20:03:29 -0600

    Project working, Angular missing

--- final/controllers/documentController.js
+++ final/controllers/documentController.js
@@ -34,50 +34,73 @@ controllers.addDocument = async (req, res) => {
     title: req.body.title,
     content: req.body.content
   };
-  const response = await mongodb.getDb().db('WordProc').collection('documents').insertOne(document);
-  if (response.acknowledged) {
-    let message = "Success!"
-    res.status(200).render('./successPage.ejs', {message} );
 
-  } else {
-    let message = "Error! " + response.error
-    res.status(500).render('./successPage.ejs', {message} );
-  }};
+  try {
+    const response = await mongodb.getDb().db('WDD430Final').collection('documents').insertOne(document);
+    const result = await mongodb.getDb().db('WDD430Final').collection('documents').find();
+    const documents = await result.toArray();
 
-
-controllers.editDocument = async (req, res, next) => {
-    try {
-      const id = new ObjectId(req.params.id);
-      const Newdoc = {
-        title: req.body.title,
-        content: req.body.content
-      };
-  
-      const response = await mongodb.getDb().db('WordProc').collection('documents').replaceOne({ _id: id }, Newdoc);
-  
-      if (response.modifiedCount > 0) {
-        let message = "Success!";
-        res.status(200).render('./successPage.ejs', {message} );
-      } else {
-        let message = "Error! Nothing happened.";
-        res.status(500).render('./successPage.ejs', {message} );
-      }
-    } catch (error) {
-      let message = "Error! " + error;
-      res.status(500).render('./successPage.ejs', {message} );
-    }
-  };
-
-  controllers.deleteDocument = async (req, res, next) =>{
-    const id = new ObjectId(req.params.id);
-    const response = await mongodb.getDb().db('WordProc').collection('documents').deleteOne({ _id: id });
-    if (response.deletedCount > 0) {
-      console.log(`${response}, got removed.`);
-      res.status(204).send();
+    if (response.acknowledged) {
+      let message = "Success!";
+      res.status(200).render('./documents.ejs', { message, documents });
     } else {
-      res.status(500).json(response.error || 'Error deleting.');
+      let message = "Error! " + response.error;
+      res.status(500).render('./documents.ejs', { message, documents });
     }
-  };
+  } catch (error) {
+    const result = await mongodb.getDb().db('WDD430Final').collection('documents').find();
+    const documents = await result.toArray();
+    let message = "Error! " + error;
+    res.status(500).render('./documents.ejs', { message, documents });
+  }
+};
+
+
+
+controllers.editDocument = async (req, res) => {
+  try {
+    const id = new ObjectId(req.params.id);
+    const Newdoc = {
+      title: req.body.title,
+      content: req.body.content
+    };
+
+    const response = await mongodb.getDb().db('WDD430Final').collection('documents').replaceOne({ _id: id }, Newdoc);
+    const result = await mongodb.getDb().db('WDD430Final').collection('documents').find();
+    const documents = await result.toArray();
+
+    if (response.modifiedCount > 0) {
+      let message = "Success!";
+      res.status(200).render('./documents.ejs', { message, documents });
+    } else {
+      let message = "Error! Nothing happened.";
+      res.status(500).render('./documents.ejs', { message, documents });
+    }
+  } catch (error) {
+    const result = await mongodb.getDb().db('WDD430Final').collection('documents').find();
+    const documents = await result.toArray();
+    let message = "Error! " + error;
+    res.status(500).render('./documents.ejs', { message, documents });
+  }
+};
+
+controllers.deleteDocument = async (req, res, next) => {
+  try {
+    const id = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db('WDD430Final').collection('documents').deleteOne({ _id: id });
+
+    if (response.deletedCount > 0) {
+      res.redirect('/');  // Redirect to the documents list after deletion
+    } else {
+      res.status(404).send('Document not found');
+    }
+  } catch (error) {
+    console.error('Delete error:', error);
+    res.status(500).send('Error deleting document');
+  }
+};
+
+
 
 // EXPORT ---------------------------------------------------------------
 module.exports = controllers
--- final/routes/index.js
+++ final/routes/index.js
@@ -2,29 +2,36 @@ const express = require('express');
 const router = express.Router();
 const documentController = require('../controllers/documentController.js');
 const mongodb = require('../database/connect.js');
-const { ObjectId } = require('mongodb');  // <-- Fix here
-const validate = require('../middleware/validate.js');
+const { ObjectId } = require('mongodb');
 
+// Get all documents
 router.get('/', documentController.getAllDocuments);
 
-router.get('/new', (req, res) => { res.render('newDocument');});
+// Render new document form
+router.get('/new', (req, res) => {
+  res.render('newDocument');
+});
 
+// Add a new document
 router.post('/', documentController.addDocument);
 
+// Render edit document form
 router.get('/:id/edit', async (req, res) => {
   const documentId = req.params.id;
-  const db = mongodb.getDb().db('WordProc');
-  const document = await db.collection('documents').findOne({ _id: new ObjectId(documentId) });  // <-- Fix here
+  const db = mongodb.getDb().db('WDD430Final'); // ✅ correct DB name
+  const document = await db.collection('documents').findOne({ _id: new ObjectId(documentId) });
 
   if (document) {
     res.render('editDocument', { document });
   } else {
     res.status(404).send('Document not found');
   }
-});  
+});
 
+// Update a document
 router.put('/:id', documentController.editDocument);
 
+// Delete a document
 router.delete('/:id', documentController.deleteDocument);
 
 module.exports = router;
